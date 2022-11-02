422fa2c2e215e0e7b3667fa7166a1686e480d615
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-11-02 14:34:06 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1,"Mr.Chocolate", 4, "M", "chocolate","hotheaded",1);
+
+INSERT INTO bears VALUES (2,"Rowdy",4,"F","chocolate","hotheaded",1);
+INSERT INTO bears VALUES (3,"Tabitha",4,"F","chocolate","hotheaded",1);
+INSERT INTO bears VALUES (4,"Sergeant Brown",4,"M","chocolate","hotheaded",1);
+INSERT INTO bears VALUES (5,"Melissa",4,"F","chocolate","hotheaded",1);
+INSERT INTO bears VALUES (6,"Grinch",4,"M","chocolate","hotheaded",1);
+INSERT INTO bears VALUES (7,"Wendy",4,"F","chocolate","hotheaded",1);
+INSERT INTO bears VALUES (8,null,4,"M","chocolate","hotheaded",1)
